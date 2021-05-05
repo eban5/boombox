5c3b0eb12ec829018e1b4dee927e93bd036bc15c
Esteban Amas <estebanamas@gmail.com>
2021-05-04 23:45:31 -0400

    Photo credit and height

--- src/PhotoCredit.tsx
+++ src/PhotoCredit.tsx
@@ -1,6 +1,6 @@
 const PhotoCredit = () => {
 	return (
-		<div>
+		<div style={{ color: "lightgray", fontSize: "0.6em" }}>
 			Photo by{" "}
 			<a href="https://unsplash.com/@vledov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
 				Vasily Ledovsky
