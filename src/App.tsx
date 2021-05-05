142ef55acdbd645815856bba442dfea2785bd4b2
Esteban Amas <estebanamas@gmail.com>
2021-05-04 23:40:01 -0400

    Controls and play state, white fill record

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import discWhite from "./disc-white.svg";
 import Controls from "./Controls";
 import Metadata from "./Metadata";
 import "./App.css";
 import PhotoCredit from "./PhotoCredit";
-import { RadioBrowserApi, Station } from "radio-browser-api";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-	// const api = new RadioBrowserApi(fetch.bind(window), "My Radio App");
+	const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
-	// const [stationList, setStationList] = useState<Station[]>([]);
-
-	// useEffect(() => {
-	// 	api.searchStations({
-	// 		limit: 50,
-	// 		tagList: ["lofi", "lofi hiphop", "lo-fi", "chill hiphop"],
-	// 	}).then((response) => {
-	// setStationList(response);
-	// 	});
-	// }, []);
-
+	const logoSpin: string = isPlaying ? "App-logo-playing" : "";
 	return (
 		<div className="App">
 			<header className="App-header">
 				<div className="brand">
-					<img src={discWhite} className="App-logo" alt="logo" />
+					<img src={discWhite} className={`App-logo ${logoSpin}`} alt="logo" />
 					<h1>boombox</h1>
 				</div>
 				<Metadata />
 
-				<FontAwesomeIcon icon={faPlayCircle} />
-				<Controls />
+				<Controls isPlaying={isPlaying} onPlayClick={setIsPlaying} />
 			</header>
 
-			{/* {stationList &&
-				stationList.map((station: Station, index: number) => {
-					return <span key={index}>{station.name}</span>;
-				})} */}
 			<PhotoCredit />
 		</div>
 	);
--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,9 +1,59 @@
-const Controls = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faPlayCircle,
+	faPauseCircle,
+	faFastForward,
+	faFastBackward,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Controls.css";
 
-export default Controls
+interface ControlsProps {
+	isPlaying: boolean;
+	onPlayClick: Function;
+}
+const Controls = (props: ControlsProps) => {
+	const { isPlaying, onPlayClick } = props;
+
+	return (
+		<>
+			{" "}
+			<div className="buttons">
+				<div className="control-icon">
+					<FontAwesomeIcon icon={faFastBackward} />
+				</div>
+				<div className="control-icon">
+					{!isPlaying ? (
+						<FontAwesomeIcon
+							icon={faPlayCircle}
+							onClick={() => onPlayClick(!isPlaying)}
+						/>
+					) : (
+						<FontAwesomeIcon
+							icon={faPauseCircle}
+							onClick={() => onPlayClick(!isPlaying)}
+						/>
+					)}
+				</div>
+				<div className="control-icon">
+					<FontAwesomeIcon icon={faFastForward} />
+				</div>
+			</div>
+			<div className="progress">
+				<input
+					type="range"
+					// value={0}
+					step="1"
+					min="0"
+					// max={duration ? duration : `${duration}`}
+					className="progress-range"
+					// onChange={(e) => onScrub(e.target.value)}
+					// onMouseUp={onScrubEnd}
+					// onKeyUp={onScrubEnd}
+					// style={{ background: trackStyling }}
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Controls;
